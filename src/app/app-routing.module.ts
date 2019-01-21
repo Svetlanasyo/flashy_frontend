d1a14583f666f6dc88cda7c0953887f01ecef410
WojciechRekosiewicz <wojtek24a@gmail.com>
2019-01-21 13:51:00 +0100

    added home component and set redirect from localhost to home.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {UserRegistrationComponent} from "./user-registration/user-registration.component";
 import {UserLoginComponent} from "./user-login/user-login.component";
+import {HomeComponent} from "./home/home.component";
 
 const routes: Routes = [
     {path: 'register', component: UserRegistrationComponent},
-    {path: 'login', component: UserLoginComponent}
+    {path: 'login', component: UserLoginComponent},
+    {path: 'home', component: HomeComponent},
+    {path: '', redirectTo: 'home', pathMatch: 'full'}
 ];
 
 @NgModule({
@@ -13,4 +16,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const routingComponents = [UserRegistrationComponent, UserLoginComponent];
+export const routingComponents = [UserRegistrationComponent, UserLoginComponent, HomeComponent];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppComponent } from './app.component';
 // import { UserRegistrationComponent } from './user-registration/user-registration.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
+// import { HomeComponent } from './home/home.component';
 // import { UserLoginComponent } from './user-login/user-login.component';
 
 @NgModule({
@@ -16,6 +17,7 @@ import { FooterComponent } from './footer/footer.component';
     // UserRegistrationComponent,
     NavbarComponent,
     FooterComponent,
+    // HomeComponent,
     // UserLoginComponent
   ],
   imports: [
